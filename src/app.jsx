32126ebdd63be609092dc93401483b6ade536b8b
Chanwooong2 <mcw0219@gmail.com>
2021-06-01 00:27:38 +0900

    React-router 테스트

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import './app.css';
+import Home from './components/home';
+import Profile from './components/profile';
 
 function App() {
-  return <h1>hello :)</h1>
+  return <BrowserRouter>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/profile">Profile</Link>
+    </nav>
+    <Switch>
+      <Route path={['/','/home']} exact>
+        <Home/> 
+        </Route>
+        {/* 성능적으로 이게 낫다 */}
+      <Route path="/profile">
+        <Profile/>
+      </Route>
+    </Switch>
+  </BrowserRouter>
+  
 }
 
 export default App;
